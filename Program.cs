8fa39007cac3785d94bb9acc6da2ee77296cfd3b
Amir <117419724+abaharloo@users.noreply.github.com>
2024-05-23 19:53:17 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("the number 2 times the entered number equals to: " + Tavan(number));
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+    public static int Tavan(int number)
+    {
+        if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+           return 2 * Tavan(number - 1);
+        }
+    }
+}
